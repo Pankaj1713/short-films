794651b991d8fb3732eba04bc17c2a7a992918c2
Pankaj Thakur <pankaj@dev-story.com>
2025-04-08 16:12:57 +0530

    latest code

--- src/Pages/home/components/heroSection/index.jsx
+++ src/Pages/home/components/heroSection/index.jsx
@@ -140,7 +140,6 @@ const HeroSection = () => {
         </motion.div>
       </AnimatePresence>
 
-      {/* Overlay Content */}
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
